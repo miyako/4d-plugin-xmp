9f2ecc28be1061614d22d6c63d8fbf5cc57ba3dc
Keisuke Miyako <keisuke.miyako@4D.com>
2023-05-15 03:16:28 +0900

    project template

---
+++ xmp/build/XMP.build/Development/XMP.build/Objects-normal/arm64/4DPlugin-XMP.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/miyako/Documents/GitHub/4d-plugin-xmp/xmp/4DPlugin-XMP.cpp \
+  /Users/miyako/Documents/GitHub/4d-plugin-xmp/xmp/4DPlugin-XMP.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-xmp/xmp/4D\ Plugin\ API/4DPluginAPI.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-xmp/xmp/4D\ Plugin\ API/Flags.h \
+  /Users/miyako/Documents/GitHub/4d-plugin-xmp/xmp/4D\ Plugin\ API/PublicTypes.h \
+  include/exempi-2.0/exempi/xmp.h
